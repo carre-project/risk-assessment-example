9b1601212a044017f51d7240b43497dfebf13b3f
Nick Portokallidis <portokallidis@gmail.com>
2016-05-30 13:07:47 +0300

    add bootstrap styling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ angular.module('CarreExample', ['ngCookies'])
 
     //set up the urls 
     var CARRE_DEVICES = API.accounts;
-        //clean up the browser url
+    //clean up the browser url
     $location.url('/').replace();
     var baseUrl = $location.absUrl();
     $scope.loginUrl = CARRE_DEVICES + '/login?next=' + baseUrl;
@@ -14,158 +14,167 @@ angular.module('CarreExample', ['ngCookies'])
 
 
     // Retrieving a cookie and set initial user object
-    API.user().then(function(res){
-        console.debug(res);
-        $scope.user = {
-          oauth_token: res.data.token,
-          username: res.data.username
-        };
+    API.user().then(function(res) {
+      $scope.user = {
+        oauth_token: res.oauth_token,
+        username: res.username
+      };
+      $scope.loadData();
     });
-    
-    $scope.loadData = function(){
-       $("#loading").show();
-      //   clear elements
-        $('#risk_factors').empty();
-        $('#measurements').empty();
+
+    $scope.loadData = function() {
+      $scope.loading = true;
       getMeasureListWithLatestValue();
     }
-    $scope.loadTestUser = function(){
-      $scope.user={
-        oauth_token:'66efc31e652208e257c3781b2a40376084c0a2ac',
-        username:'duthteam',
+    $scope.loadTestUser = function() {
+      $scope.user = {
+        oauth_token: '66efc31e652208e257c3781b2a40376084c0a2ac',
+        username: 'duthteam',
       }
       $scope.loadData();
     }
-    
-    
-var results = {};
 
 
-function getMeasureListWithLatestValue() {
-  if(!$scope.user.username) return;
-  API.lastMeasurements($scope.user).then(function(res){
-    console.log(res);
-    var data = res.data;
-          
-    results.predicates=[];     
-    results.values={};
-    results.observable_names={};
-    results.ob_dates={};
-      
-    data.map(function(obj){
-      results.predicates.push(obj.p.value.replace("http://carre.kmi.open.ac.uk/ontology/sensors.owl#",":"));    
-      results.values[makeLabel(obj.ob.value)]=obj.value.value;
-      results.observable_names[makeLabel(obj.ob.value)]=obj.ob_name.value;
-      results.ob_dates[makeLabel(obj.ob.value)]=obj.date.value;
-    }); 
-  
-    //get the risk evidences
-    getRiskEvidences()
-      
-  },function(err) { console.log("Error in query measurementList"); console.log(err); })
-  
-    
-};
-  
+    var results = {};
 
-function getRiskEvidences() {
-  
-  API.risk_evidences($scope.user,results.predicates).then(function(res){
-    var data = res.data;
-          results.summary=[];
-          results.risk_evidences=[];
-          results.risk_factors={};
-          results.total_risk_evidences=data.length;
-          data.forEach(function(rv){   
-            var result = RiskEvidenceConditionParser.evaluate(rv.condition.value,results.values);
 
-//             console.log(
-//               makeLabel(rv.risk_factor.value),
-//               makeLabel(rv.risk_evidence.value),
-//               result,
-//               rv.condition.value,getValues(results.values).join("|"),
-//               "---------"
-//             );
-            
-            
-           if(result) {
-             
-            var risk_factor=makeLabel(rv.risk_factor.value);
-            var risk_evidence=makeLabel(rv.risk_evidence.value);
-            var rf_label=makeLabel(rv.rl_source_name.value)+" "+
-                makeLabel(rv.has_risk_factor_association_type.value)+" "+
-                makeLabel(rv.rl_target_name.value);
+    function getMeasureListWithLatestValue() {
 
-            results.risk_factors[risk_factor] =results.risk_factors[risk_factor] || {label:rf_label,evidences:[]}; 
-             
-             results.risk_factors[risk_factor].evidences.push(risk_evidence); 
-             results.risk_evidences.push(risk_evidence);
-             
-           }
-            
+      if (!$scope.user.username) return;
+      API.lastMeasurements($scope.user).then(function(res) {
+        console.log(res);
+        var data = res.data;
+
+        results.predicates = [];
+        results.values = {};
+        results.observable_names = {};
+        results.ob_dates = {};
+
+        data.map(function(obj) {
+          results.predicates.push(obj.p.value.replace("http://carre.kmi.open.ac.uk/ontology/sensors.owl#", ":"));
+          results.values[makeLabel(obj.ob.value)] = obj.value.value;
+          results.observable_names[makeLabel(obj.ob.value)] = obj.ob_name.value;
+          results.ob_dates[makeLabel(obj.ob.value)] = obj.date.value;
+        });
+
+        //get the risk evidences
+        getRiskEvidences()
+
+      }, function(err) {
+        console.log("Error in query measurementList");
+        console.log(err);
+      })
+
+
+    };
+
+
+    function getRiskEvidences() {
+
+      API.risk_evidences($scope.user, results.predicates).then(function(res) {
+        var data = res.data;
+        results.summary = [];
+        results.risk_evidences = {};
+        results.risk_factors = {};
+        results.total_risk_evidences = data.length;
+        data.forEach(function(rv) {
+          var result = RiskEvidenceConditionParser.evaluate(rv.condition.value, results.values);
+
+          //             console.log(
+          //               makeLabel(rv.risk_factor.value),
+          //               makeLabel(rv.risk_evidence.value),
+          //               result,
+          //               rv.condition.value,getValues(results.values).join("|"),
+          //               "---------"
+          //             );
+
+
+          if (result) {
+
+            var risk_factor = makeLabel(rv.risk_factor.value);
+            var risk_evidence = makeLabel(rv.risk_evidence.value);
+            var rf_label = makeLabel(rv.rl_source_name.value) + " " +
+              makeLabel(rv.has_risk_factor_association_type.value) + " " +
+              makeLabel(rv.rl_target_name.value);
+
+            results.risk_factors[risk_factor] = results.risk_factors[risk_factor] || {
+              label: rf_label,
+              evidences: []
+            };
+
+            results.risk_factors[risk_factor].evidences.push(risk_evidence);
+            results.risk_evidences[risk_evidence] = {
+              confidence_interval_min: rv.confidence_interval_min.value,
+              confidence_interval_max: rv.confidence_interval_max.value,
+              risk_evidence_ratio_value: rv.risk_evidence_ratio_value.value
+            };
+
+          }
+
+        })
+        console.log(results);
+        display(results);
+        //           console.log(
+        //             results.risk_factors,
+        //             results.values,
+        //             results.summary.length+"/"+results.total_risk_evidences);
+
+      });
+
+    };
+
+    function display(results) {
+
+      $scope.risk_factors = [];
+      //make risk factors with evidences
+      for (var rf in results.risk_factors) {
+        $scope.risk_factors.push({
+          label: results.risk_factors[rf].label,
+          link: "https://entry.carre-project.eu/risk_factors/" + rf,
+          evidences: results.risk_factors[rf].evidences.map(function(ev) {
+            return {
+              link: "https://entry.carre-project.eu/risk_evidences/" + ev,
+              label: ev,
+              ratio: results.risk_evidences[ev].risk_evidence_ratio_value
+            };
           })
-          console.log(results);
-          display(results);
-//           console.log(
-//             results.risk_factors,
-//             results.values,
-//             results.summary.length+"/"+results.total_risk_evidences);
-          
-        },
-        error : function(err) {
-                console.log("Error in query RiskEvidences");
-                console.log(err);
-        }
-    });
+        });
+      }
+
+      $scope.measurements = [];
+      //make measurements
+      for (var ob in results.observable_names) {
+        $scope.measurements.push({
+          label: results.observable_names[ob],
+          link: "https://entry.carre-project.eu/observables/" + ob,
+          value: results.values[ob],
+          date: new Date(results.ob_dates[ob]).toLocaleString()
+        });
+      }
+
+      $scope.loading = false;
+
+    }
+
+
+    function makeLabel(str) {
+      var result = "";
+      if (str.indexOf("#") >= 0) {
+        result = str.substring(str.lastIndexOf("#") + 1)
+          .replace("risk_factor_association_type", "")
+      }
+      else result = str.substring(str.lastIndexOf("/") + 1);
+      if (result.indexOf("RF_") +
+        result.indexOf("OB_") +
+        result.indexOf("RV_") +
+        result.indexOf("RL_") > -4) return result;
+      else return result.replace(/[_-]+/g, " ");
+    }
     
-};
 
-function display(results){
+  })
   
-    //   clear elements
-    $('#risk_factors').empty();
-    $('#measurements').empty();
-  //make risk factors with evidences
-  for (var rf in results.risk_factors) {
-    var evidences=[];
- results.risk_factors[rf].evidences.forEach(function(ev){ evidences.push('<a target="_blank" href="https://entry.carre-project.eu/risk_evidences/'+ev+'">'+ev+'</a>'); });
-    
-   $('#risk_factors').append('<li><a target="_blank" href="https://entry.carre-project.eu/risk_factors/'+rf+'">' +results.risk_factors[rf].label+ '</a> with evidences: '+evidences.join(",")+'</li>');
-  }
-  
-  //make measurements
-  for (var ob in results.observable_names) {
-  $('#measurements').append(
-    '<li><a target="_blank" href="https://entry.carre-project.eu/observables/'+ob+'">' +results.observable_names[ob]+ '</a>: '+results.values[ob] +'  <small>   [ '+
-    new Date(results.ob_dates[ob]).toLocaleString()+'] </small> </li>'
-  )
-  }
-  
-  
-       $("#loading").hide();
-  
-}
-
-function getValues (o) {
-  var arr=[];
-  for (var p in o){ arr.push(p+"="+o[p])}
-  return arr;
-}
-function makeLabel(str) { 
-  var result="";
-  if(str.indexOf("#")>=0) {
-    result=str.substring(str.lastIndexOf("#")+1)
-      .replace("risk_factor_association_type","")
-  } else result = str.substring(str.lastIndexOf("/")+1);
-  if(result.indexOf("RF_")+
-     result.indexOf("OB_")+
-     result.indexOf("RV_")+
-     result.indexOf("RL_")>-4) return result;
-  else return result.replace(/[_-]+/g, " ");
-}
-
-
-  }).service('API', function($http, $cookies) {
+  .service('API', function($http, $cookies, $q) {
 
     //set up the urls 
     var CARRE_DEVICES = 'https://devices.carre-project.eu/devices/accounts';
@@ -200,7 +209,11 @@ function makeLabel(str) {
         console.log(err);
         return {}
       });
-    } else return;
+    } else {
+      var r=$q.defer();
+      r.reject({})
+      return r.promise;
+    }
   }
   
   
@@ -221,7 +234,7 @@ function makeLabel(str) {
     FILTER (lang(?ob_name)='en') \n\
     } \n";
 
-    return $http.post(URL+'/query',{token:user.oauth_token,sparql:encodeURIComponent(query)});
+    return $http.post(URL+'query?token='+user.oauth_token+'&sparql='+encodeURIComponent(query));
     
 
   }
@@ -258,7 +271,7 @@ function makeLabel(str) {
     " }";
 
 
-    return $http.post(URL+'/query',{token:user.oauth_token,sparql:encodeURIComponent(query)});
+    return $http.post(URL+'query?token='+user.oauth_token+'&sparql='+encodeURIComponent(query));
     
 
   }
